69f8051acbee431f9639a8ea334765a044fb1dc7
Marcus Kida <marcus@kida.io>
2019-11-02 09:51:39 +0100

    Initial Commit

---
+++ KeychainWrapperDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeychainWrapperDemo
+//
+//  Created by Marcus Kida on 02.11.19.
+//  Copyright © 2019 Bearologics UG (haftungsbeschränkt). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
